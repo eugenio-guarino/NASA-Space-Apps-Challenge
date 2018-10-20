43d4c9732c3c627e82e1923362b1867c18923bd8
Goel Biju <goel@compsoc>
2018-10-20 15:26:40 +0100

    Added LivingQuality files

---
+++ LivingQuality/LivingQuality/LivingQuality/Models/AirQualityAPI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using RestSharp;
+
+namespace LivingQuality.Models
+{
+    class AirQualityAPI
+    {
+        // API Documentation: https://aqicn.org/json-api/doc/
+        private string api_Url = "https://api.waqi.info";
+
+        // city and token
+        private string api_CityFeedUrl = "https://api.waqi.info/feed/{0}/?token={1}";
+        // lat, long and token
+        private string api_GeolocalisedUrl = "https://api.waqi.info/feed/geo:{0};{1}/?token={2}";
+        // token and keyword
+        private string api_Search = "https://api.waqi.info/search/?token={0}&keyword={1}";
+
+        private string apiToken = null;
+
+        public AirQualityAPI()
+        {
+            // Store the token we need to use the AirQuality
+            apiToken = "454cac0b25995e1a4566c412dab345ddacbea0fa";
+        }
+
+        public void GetData()
+        {
+            var client = new RestClient(api_Url);
+
+            var request = new RestRequest("feed/{city}", Method.GET);
+            request.AddParameter("token", apiToken);
+            request.AddUrlSegment("city", "Plymouth");
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            Console.WriteLine(content);
+        }
+
+    }
+}
